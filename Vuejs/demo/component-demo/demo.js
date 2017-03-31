586433bc80bc1b028c7a1db7651a1bc9ef08dcb1
desen <desen@mogujie.com>
2017-03-31 19:32:40 +0800

    update hahaha

--- Vuejs/demo/component-demo/demo.js
+++ Vuejs/demo/component-demo/demo.js
@@ -3,28 +3,33 @@
  */
 
 //注册一个 全局 组件
-Vue.component('child',{
-    props:['message'],
-    template:'<div>我来自子组件,{{message}}</div>'
+Vue.component('child', {
+    props: ['message'],
+    template: '<div>我来自子组件,{{message}}</div>'
 });
 
 //常用》 注册一个局部>> 局部是指只有在父元素可用
 var ChildTwo = {
-    template:'<div><slot></slot>我来自子组件，我是老二</div>'
+    template: '<div><slot></slot>我来自子组件，我是老二.{{info}}</div>',
+    data: function(){
+        return {
+            info: 'info'
+        }
+    }
 };
 new Vue({
-    el:'#cls',
-    components:{
-        "child-two":ChildTwo
+    el: '#cls',
+    components: {
+        "child-two": ChildTwo
 
     },
-    data:function () {
+    data: function () {
         return {
 
         }
     },
-    methods:{
-        hello:function () {
+    methods: {
+        hello: function () {
             alert(1)
         }
     }
